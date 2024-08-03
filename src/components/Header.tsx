563a932b63d9eaed4d835694d312f3c1fece2619
Samarth <samarthk003@gmail.com>
2024-08-03 23:26:07 +0530

    minor fixes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -16,7 +16,6 @@ const Header: FunctionComponent = () => {
                     <div className="search-bar">
                         <input id="search-input" type="text" placeholder="Search" />
                         <button type="submit" className="search-button"><i className="fa fa-search"></i></button>
-
                     </div>
                 </div>
             </div>
